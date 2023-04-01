1568e26167bdfec1964f85d14b37b2955037e452
abc <abc@example.com>
2023-04-01 18:59:48 +0530

    first commit

---
+++ src/main/java/com/example/anotherservice/AnotherserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.anotherservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotherserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnotherserviceApplication.class, args);
+	}
+
+}
